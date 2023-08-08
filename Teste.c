ac03ba80bf7b0c417fe6515a9e30fc681c0a6147
aluno <bruno.hp2002@aluno.ifsc.edu.br>
2023-08-08 15:05:54 -0300

    Teste.c

---
+++ Teste.c
@@ -0,0 +1,11 @@
+//
+// Created by aluno on 08/08/23.
+//
+#include <stdio.h>
+#include "Teste.h"
+
+int main (){
+    printf("Hello World!\n");
+
+    return 0;
+}
---
+++ Teste.h
@@ -0,0 +1,8 @@
+//
+// Created by aluno on 08/08/23.
+//
+
+#ifndef PRG2_TESTE_H
+#define PRG2_TESTE_H
+
+#endif //PRG2_TESTE_H
